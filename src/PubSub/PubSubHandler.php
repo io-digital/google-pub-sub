1a07420e3b3ca4aba8db51d1ab43365416eff170
Ian van der Merwe <ian@io.co.za>
2016-04-19 15:09:46 +0200

    added publish method

--- src/PubSub/PubSubHandler.php
+++ src/PubSub/PubSubHandler.php
@@ -1,30 +1,32 @@
 <?php namespace IoDigital\GooglePubSub\PubSub;
 
 use IoDigital\GooglePubSub\PubSub\Google_Service\Google_Service_Pubsub;
+use IoDigital\GooglePubSub\PubSub\Google_Service\Google_Service_Pubsub_PublishRequest;
+use IoDigital\GooglePubSub\PubSub\Google_Service\Google_Service_Pubsub_PubsubMessage;
 
 class PubSubHandler
 {
     protected $googlePubSubService;
     protected $google_client;
 
-    protected $project_id;
-    protected $project_topic;
-
     protected $topicStub;
     protected $subStub;
+    protected $pubStub;
     /**
      * PubSub constructor.
      */
     public function __construct(GoogleClient $googleClient)
     {
         $this->topicStub = 'projects/'. $googleClient->getApplicationName() .'/topics/';
+
         $this->subStub = 'projects/'. $googleClient->getApplicationName() .'/subscriptions/';
+        $this->pubStub = 'projects/'. $googleClient->getApplicationName() .'/topics/';
 
         $this->google_client = $googleClient->getClient();
         $this->googlePubSubService = new Google_Service_Pubsub($this->google_client);
     }
 
-    public function subToTopic($topic,$subName)
+    public function subToTopic($topic, $subName)
     {
         $subService = $this->googlePubSubService->projects_subscriptions;
 
@@ -36,6 +38,8 @@ class PubSubHandler
 
         //projects/io-anvil/subscriptions/sub-23
         $subService->create($this->subStub . $subName,$sub);
+
+        return true;
     }
 
     public function createTopic($topicName)
@@ -45,5 +49,24 @@ class PubSubHandler
         $topic = new \Google_Service_Pubsub_Subscription();
 
         $subService->create($this->subName . $topicName,$topic);
+
+        return true;
+    }
+
+    public function publishToTopic($topicName, $messages)
+    {
+        $topicsService = $this->googlePubSubService->projects_topics;
+
+        $googleServicePubSubPublishRequest = new Google_Service_Pubsub_PublishRequest();
+
+        $message = new Google_Service_Pubsub_PubsubMessage();
+
+        $message->setData($messages);
+
+        $googleServicePubSubPublishRequest->setMessages($message);
+
+        $topicsService->publish($this->pubStub . $topicName, $googleServicePubSubPublishRequest);
+
+        return true;
     }
 }
